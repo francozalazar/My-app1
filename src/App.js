2a8d56c68ea6533a6ba00121ae77627e8cbcbeae
francozalazar <francorzalazar906@gmail.com>
2021-11-25 20:38:11 -0300

    Agregue Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavBar } from './components/NavBar'
+
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +22,10 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
+  
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+// import React from  "react"
+import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button, } from 'react-bootstrap'
+export const NavBar = () => {
+  return (
+      <Navbar bg="dark" expand="lg" variant="dark">
+          <Container>
+              <Navbar.Brand href="#">Home</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                  <Nav
+                      className="me-auto my-2 my-lg-0"
+                      style={{ maxHeight: '100px' }}
+                      navbarScroll
+                  >
+                      <Nav.Link href="#">Catálogo</Nav.Link>
+                      <NavDropdown title="Más" id="navbarScrollingDropdown">
+                          <NavDropdown.Item href="#">Metodos de pago</NavDropdown.Item>
+                          <NavDropdown.Item href="#">Entregas</NavDropdown.Item>
+                          <NavDropdown.Divider />
+                          <NavDropdown.Item href="#">
+                              Sobre nosotros
+                          </NavDropdown.Item>
+                      </NavDropdown>
+                  </Nav>
+                  <Form className="d-flex container">
+                      <FormControl
+                          type="search"
+                          placeholder="Buscar modelos..."
+                          className="me-2"
+                          aria-label="Search"
+                      />
+                      <Button variant="outline-info">Buscar</Button>
+                  </Form>
+                  
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
+
+  )
+}
+
