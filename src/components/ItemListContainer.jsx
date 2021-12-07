393f415183150affd8f808b8b8a1cfbe4f3c5cde
francozalazar <francorzalazar906@gmail.com>
2021-12-07 15:33:16 -0300

    Agregue Contador

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import ItemCount from "./ItemCount"
 
 const ItemListContainer = (props) => {
     return (
         <>
-            <h1 className="text-primary d-flex justify-content-center align-items-center">{props.productos}</h1>
+            <h1 className="text-info d-flex justify-content-center align-items-center">{props.productos}</h1>
+            <ItemCount/>
         </>
+    
     )
 }
 export default ItemListContainer
